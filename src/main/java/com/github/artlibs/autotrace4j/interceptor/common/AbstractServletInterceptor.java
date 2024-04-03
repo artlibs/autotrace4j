b4f2d9e884b8f6b05c1f9b87a9ccbb7269833c58
suopovate <806019582@qq.com>
2024-04-03 16:14:29 +0800

    1. optimize AbstractServletInterceptor.java 2. change source code version to java 21

--- src/main/java/com/github/artlibs/autotrace4j/interceptor/common/AbstractServletInterceptor.java
+++ src/main/java/com/github/artlibs/autotrace4j/interceptor/common/AbstractServletInterceptor.java
@@ -1,8 +1,8 @@
 package com.github.artlibs.autotrace4j.interceptor.common;
 
-import com.github.artlibs.autotrace4j.interceptor.base.AbstractInstanceInterceptor;
 import com.github.artlibs.autotrace4j.context.AutoTraceCtx;
 import com.github.artlibs.autotrace4j.context.ReflectUtils;
+import com.github.artlibs.autotrace4j.interceptor.base.AbstractInstanceInterceptor;
 import com.github.artlibs.autotrace4j.support.Constants;
 
 import java.lang.reflect.Method;
@@ -45,16 +45,15 @@ public abstract class AbstractServletInterceptor extends AbstractInstanceInterce
                 .invoke(AutoTraceCtx.ATO_TRACE_ID);
             String parentSpanId = ReflectUtils
                 .getMethodWrapper(allArgs[0], Constants.GET_ATTRIBUTE, Object.class)
+                .invoke(AutoTraceCtx.ATO_PARENT_SPAN_ID);
+            String spanId = ReflectUtils
+                .getMethodWrapper(allArgs[0], Constants.GET_ATTRIBUTE, Object.class)
                 .invoke(AutoTraceCtx.ATO_SPAN_ID);
+
             if (Objects.nonNull(traceId)) {
-                ReflectUtils
-                    .getMethodWrapper(allArgs[0], Constants.SET_ATTRIBUTE, String.class, Object.class)
-                    .invoke(AutoTraceCtx.ATO_TRACE_ID, traceId);
-                ReflectUtils
-                    .getMethodWrapper(allArgs[0], Constants.SET_ATTRIBUTE, String.class, Object.class)
-                    .invoke(AutoTraceCtx.ATO_SPAN_ID, parentSpanId);
                 AutoTraceCtx.setTraceId(traceId);
                 AutoTraceCtx.setParentSpanId(parentSpanId);
+                AutoTraceCtx.setSpanId(spanId);
             } else {
                 traceId = ReflectUtils
                     .getMethodWrapper(allArgs[0], Constants.GET_HEADER, String.class)
@@ -63,35 +62,33 @@ public abstract class AbstractServletInterceptor extends AbstractInstanceInterce
                     .getMethodWrapper(allArgs[0], Constants.GET_HEADER, String.class)
                     .invoke(AutoTraceCtx.ATO_SPAN_ID);
 
-                boolean setTraceIdToResponse = true;
-                if (Objects.isNull(traceId)) {
-                    traceId = ReflectUtils
-                        .getMethodWrapper(allArgs[1], Constants.GET_HEADER, String.class)
-                        .invoke(AutoTraceCtx.ATO_TRACE_ID);
-                    parentSpanId = ReflectUtils
-                        .getMethodWrapper(allArgs[1], Constants.GET_HEADER, String.class)
-                        .invoke(AutoTraceCtx.ATO_SPAN_ID);
-
-                    setTraceIdToResponse = Objects.isNull(traceId);
-                }
-
                 if (Objects.isNull(traceId)) {
                     traceId = AutoTraceCtx.generate();
                 }
-                final String spanId = AutoTraceCtx.generate();
+                spanId = AutoTraceCtx.generate();
 
                 AutoTraceCtx.setSpanId(spanId);
                 AutoTraceCtx.setTraceId(traceId);
                 AutoTraceCtx.setParentSpanId(parentSpanId);
 
-                if (setTraceIdToResponse) {
-                    ReflectUtils
-                        .getMethodWrapper(allArgs[1], Constants.SET_HEADER, String.class, String.class)
-                        .invoke(AutoTraceCtx.ATO_TRACE_ID, traceId);
-                    ReflectUtils
-                        .getMethodWrapper(allArgs[1], Constants.SET_HEADER, String.class, String.class)
-                        .invoke(AutoTraceCtx.ATO_SPAN_ID, spanId);
-                }
+                ReflectUtils
+                    .getMethodWrapper(allArgs[0], Constants.SET_ATTRIBUTE, String.class, Object.class)
+                    .invoke(AutoTraceCtx.ATO_TRACE_ID, traceId);
+                ReflectUtils
+                    .getMethodWrapper(allArgs[0], Constants.SET_ATTRIBUTE, String.class, Object.class)
+                    .invoke(AutoTraceCtx.ATO_PARENT_SPAN_ID, parentSpanId);
+                ReflectUtils
+                    .getMethodWrapper(allArgs[0], Constants.SET_ATTRIBUTE, String.class, Object.class)
+                    .invoke(AutoTraceCtx.ATO_SPAN_ID, spanId);
+                ReflectUtils
+                    .getMethodWrapper(allArgs[1], Constants.SET_HEADER, String.class, String.class)
+                    .invoke(AutoTraceCtx.ATO_TRACE_ID, traceId);
+                ReflectUtils
+                    .getMethodWrapper(allArgs[1], Constants.SET_HEADER, String.class, String.class)
+                    .invoke(AutoTraceCtx.ATO_PARENT_SPAN_ID, parentSpanId);
+                ReflectUtils
+                    .getMethodWrapper(allArgs[1], Constants.SET_HEADER, String.class, String.class)
+                    .invoke(AutoTraceCtx.ATO_SPAN_ID, spanId);
             }
         }
     }
